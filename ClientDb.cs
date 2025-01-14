5555d206d06c5026265942c6e2ec0e411140a171
joydfr <dufour.jody@orange.fr>
2025-01-14 10:05:51 +0100

    ajout commentaire dans program.cs et ClientDB, version 2

--- ClientDb.cs
+++ ClientDb.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Text;
+using Microsoft.EntityFrameworkCore;
 
+//Créer une classe ClientsDb qui hérite de DbContext sur la base de données Entity Framework Core.
 class ClientsDb : DbContext
 {
+    //Définir un constructeur pour `ClientsDb`
+    // Le constructeur accepte un paramètre options (de type `DbContextOptions<ClientsDb>`).
     public ClientsDb(DbContextOptions<ClientsDb> options) : base(options) { }
+    //- Représente la table des clients dans la base de données.Chaque client correspond à une instance
+    //de la classe Clients.
     public DbSet<Clients> Clients => Set<Clients>();
     public DbSet<Categories> Categories => Set<Categories>();
     public DbSet<Items> Items => Set<Items>();
@@ -11,15 +17,24 @@ class ClientsDb : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        //Définir les relations pour Categories :
         modelBuilder.Entity<Categories>()
+            //Une catégorie  peut avoir plusieurs articles
             .HasMany(c => c.Items)
+            //Un article appartient à une catégorie (relation 1:N).
             .WithOne()
+            //La clé étrangère pour cette relation est CategoryId.
             .HasForeignKey(i => i.CategoryId)
+            //chaque article doit obligatoirement avoir une catégorie.
             .IsRequired();
 
+        //Définir les relations pour OrderItem :
         modelBuilder.Entity<OrderItem>()
+            //Un article de commande est lié à un seul article.
             .HasOne<Items>()
+            //Un article peut être lié à plusieurs articles de commande (relation Many to One).
             .WithMany()
+            //La clé étrangère pour cette relation est ItemId.
             .HasForeignKey(oi => oi.ItemId);
 
         modelBuilder.Entity<Order>()
